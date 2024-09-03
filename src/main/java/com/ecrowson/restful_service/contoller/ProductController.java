dece29d8c4de6e2913c0d5a0ee1959e07d8af661
Ethan Crowson <ethancrowson@hotmail.co.uk>
2024-09-03 22:17:16 +0100

    Get Mappings for products

--- src/main/java/com/ecrowson/restful_service/contoller/ProductController.java
+++ src/main/java/com/ecrowson/restful_service/contoller/ProductController.java
@@ -5,12 +5,13 @@ import com.ecrowson.restful_service.model.Product;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
+import java.util.function.BiFunction;
 
 @RestController
 public class ProductController {
@@ -22,6 +23,21 @@ public class ProductController {
         return ResponseEntity.status(HttpStatus.CREATED).contentType(MediaType.APPLICATION_JSON).body(result);
     }
 
+    @GetMapping("/product")
+    public List<Product> getAllProducts(){
+        List<Product> result = new ArrayList<>();
+        result.add(new Product("1","Product1",new BigDecimal("258.33")));
+        result.add(new Product("2","Product2",new BigDecimal("451.99")));
+        result.add(new Product("3","Product3",new BigDecimal("634.10")));
+
+        return result;
+    }
+
+    @GetMapping("/product/{id}")
+    public Product getProductByID(@PathVariable final String id) {
+        System.out.println(id);
+        return new Product("888","Returning a single product",new BigDecimal("458.44"));
+    }
 
 
 }
